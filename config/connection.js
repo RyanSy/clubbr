4625b807411dcaa644a2b3f6151eef15695e59a3
SWERVE <swerve@CracBook-Pro.local>
2016-05-26 23:39:43 -0400

    edited connection.js

--- config/connection.js
+++ config/connection.js
@@ -7,36 +7,36 @@ var Sequelize = require("sequelize"),
   connection;
 
 // Lists out connection options
-// var source = {
-//     localhost: {
-//         host: 'localhost',
-//         user: 'root',
-//         password: "1111",
-//         database: "clubbr",
-//     }
-// }
+var source = {
+    localhost: {
+        host: 'localhost',
+        user: 'root',
+        password: "1111",
+        database: "clubbr",
+    }
+}
 
-// Selects a connection (can be changed quickly as needed)
-// var selectedSource = source.localhost;
+//Selects a connection (can be changed quickly as needed)
+var selectedSource = source.localhost;
 
-// Creates mySQL connection using Sequelize or JAWSDB
-// if (process.env.JAWSDB_URL) {
-  connection = new Sequelize(process.env.JAWSDB_URL);
-// }
-// else {
-//   var sequelize = new Sequelize(selectedSource.database, selectedSource.user, selectedSource.password, {
-//     define: { timestamps: false },
-//     host: selectedSource.host,
-//     dialect: 'mysql',
-//
-//     pool: {
-//       max: 5,
-//       min: 0,
-//       idle: 10000
-//     },
-//
-//   });
-// }
+//Creates mySQL connection using Sequelize or JAWSDB
+if (process.env.JAWSDB_URL) {
+  var sequelize = new Sequelize(process.env.JAWSDB_URL);
+}
+else {
+  var sequelize = new Sequelize(selectedSource.database, selectedSource.user, selectedSource.password, {
+    define: { timestamps: false },
+    host: selectedSource.host,
+    dialect: 'mysql',
+
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    },
+
+  });
+}
 
 // Exports the connection for other files to use
 module.exports = sequelize;
