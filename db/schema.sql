40469bc8a26c4adcda94d3769565fa8590341334
SWERVE <swerve@CracBook-Pro.local>
2016-05-27 20:25:19 -0400

    edited schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -3,22 +3,25 @@
 CREATE DATABASE clubbr;
 USE clubbr;
 
-CREATE TABLE events
-(
-	id int NOT NULL AUTO_INCREMENT,
-	user_id int NOT NULL,
-	name varchar(255) NOT NULL,
-	genre varchar(255) NOT NULL,
-	venue varchar(255) NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (user_id) REFERENCES users(id)
-);
-
 CREATE TABLE users
 (
 	id int NOT NULL AUTO_INCREMENT,
 	username varchar(255) NOT NULL,
 	email varchar(255) NOT NULL,
 	password_hash varchar(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE events
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	djs varchar(255) NOT NULL,
+	genre varchar(255) NOT NULL,
+	venue varchar(255) NOT NULL,
+	address varchar(255) NOT NULL,
+	phone varchar(255) NOT NULL,
+	hours varchar(255) NOT NULL,
+	reservations varchar(255) NOT NULL,
 	PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,14 @@
-INSERT INTO users (username, email, password_hash) VALUES ('ryansy', 'ryanbsy@gmail.com', '$2a$10$Nu4oBiV9kjMSD0sxnrhYkOLXgaHtfC3wb5xB5b8to.YtGvmVyz2ZC'); /* password is password */
+INSERT INTO users (username, email, password_hash) VALUES ('mamun', 'mamun0320@gmail.com', 'password');
+INSERT INTO users (username, email, password_hash) VALUES ('ryansy', 'ryan123@gmail.com', 'password');
+INSERT INTO users (username, email, password_hash) VALUES ('christmas', 'chris303@gmail.com', 'password');
 
-INSERT INTO users (username, email, password_hash) VALUES ('q', 'q@q.com', '$2a$10$Nu4oBiV9kjMSD0sxnrhYkOLXgaHtfC3wb5xB5b8to.YtGvmVyz2ZC'); /* password is password */
 
-INSERT INTO events (name, genre, venue) VALUES ('Roots NYC', 'house', 'cielo');
-INSERT INTO events (name, genre, venue) VALUES ('Toca Tuesdays', 'hiphop', 'the delancey');
+INSERT INTO events (name, djs, venue, address, phone, hours, reservations, genre) VALUES ("Jazz Party", 'no dj', "SOB's", 'New York, NY', 2122434940, '6PM–1AM', 'NO', 'Jazz');
+INSERT INTO events (name, djs, venue, address, phone, hours, reservations, genre)VALUES ('hip-pop party', 'FUNK FLEX', 'Santos Party House', 'New York, NY', 2125845492, '7PM–4AM', 'NO', 'hip-pop');
+INSERT INTO events (name, djs, venue, address, phone, hours, reservations, genre) VALUES ('HOUSE FRIDAYS', 'JUNIOR VASQUEZ','Le Bain', 'New York, NY', 2126457600, '10PM–4AM', 'NO', 'house');
+INSERT INTO events (name, djs, venue, address, phone, hours, reservations, genre) VALUES ('dance. here. now.', 'louie vega','Cielo', 'New York, NY', 6465438556, '10PM–4AM', 'NO', 'Meatpacking disco');
+INSERT INTO events (name, djs, venue, address, phone, hours, reservations, genre) VALUES ('freaky fridays', 'dj big ben','Marquee New York', 'New York, NY', 6464730202, '7PM–4AM', 'NO', 'Rock');
+INSERT INTO events (name, djs, venue, address, phone, hours, reservations, genre) VALUES ('tbt', 'dj red alert','Southside', 'New York, NY', 2126805601, '11PM–4AM', 'NO', 'Rock/hip-pop');
+INSERT INTO events (name, djs, venue, address, phone, hours, reservations, genre) VALUES ('wild out wednesdays', 'dj scratch','XL Nightclub', 'New York, NY', 2122392999, '7PM–4AM', 'NO', 'techno');
+INSERT INTO events (name, djs, venue, address, phone, hours, reservations, genre) VALUES ('bt night','dj nimrod','Webster Hall', 'New York, NY', 2123531600, '10PM–3AM', 'NO', 'Rock/jazz/hip-pop/house');
+INSERT INTO events (name, djs, venue, address, phone, hours, reservations, genre) VALUES ('ibiza in ny','john digweed','Haus NYC', 'New York, NY', 2126254287, '11PM–4AM', 'NO', 'international house DJs');
