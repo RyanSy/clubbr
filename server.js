1a84cc23828e5f8cd5b6a68220ade21267ec4843
SWERVE <swerve@CracBook-Pro.local>
2016-06-02 12:06:04 -0400

    added start script to package.json

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.use('/', events_controller);
 app.use('/', users_controller);
 
 
-var port = 3000;
-app.listen(port || process.env.PORT);
+var port = process.env.PORT || 3000;
+app.listen(port);
 console.log("Connected established at port:" + port);
